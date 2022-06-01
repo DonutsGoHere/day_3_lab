33f3321846e557008b83b3aa80237d30f4346dd3
Calum Cook <calumcook@Calums-Air.broadband>
2022-06-02 00:01:22 +0100

    2nd commit - followed the todo example for MVP

---
+++ models/order.py
@@ -0,0 +1,9 @@
+class Order():
+
+  def __init__(self, name, date, quantity, title, author):
+    self.name = name
+    self.date = date
+    self.quantity = quantity
+    self.itle = title
+    self.author = author
+
---
+++ models/order_list.py
@@ -0,0 +1,7 @@
+from models.order import *
+
+order1 = Order("Debbie Cortez", "1-6-22", 1, "The Forever War", "Joe Haldeman")
+order2 = Order("Howard Harmon", "1-6-22", 1, "Do Androids Dream of Electric Sheep?", "Joe Haldeman")
+order3 = Order("Dana Bell", "31-5-22", 1, "The Lord of The Rings", "J.R.R Tolkien")
+order4 = Order("Tom Luna", "30-5-22", 1, "Clean Code", "Robert C. Martin")
+orders = [order1, order2, order3, order4]
